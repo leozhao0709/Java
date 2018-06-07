3b2dbe24afbd2b6cc931e5af8e7f7e309926e7f7
Lei Zhao <zhao434@usc.edu>
2018-06-06 20:44:03 -0700

    2018-06-06 20:43:53 auto check in

--- javaSE/algorithms/leetcode/src/main/java/twosum/Solution.java
+++ javaSE/algorithms/leetcode/src/main/java/twosum/Solution.java
@@ -4,11 +4,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
-    private String abc;
 
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        this.abc = "abc";
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(target - nums[i])) {
                 return new int[]{map.get(target - nums[i]), i};
