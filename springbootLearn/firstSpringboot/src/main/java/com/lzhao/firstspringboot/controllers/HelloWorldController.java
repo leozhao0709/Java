8821e5e88685db4dd0f72c69d00a2c00b9e72e3e
Lei Zhao <zhao434@usc.edu>
2019-01-21 06:30:11 -0800

    2019-01-21 06:30:04 auto check in

--- springbootLearn/firstSpringboot/src/main/java/com/lzhao/firstspringboot/controllers/HelloWorldController.java
+++ springbootLearn/firstSpringboot/src/main/java/com/lzhao/firstspringboot/controllers/HelloWorldController.java
@@ -1,13 +1,29 @@
 package com.lzhao.firstspringboot.controllers;
 
 import com.lzhao.firstspringboot.exceptions.UserNotFindException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+import java.util.Map;
+
 @Controller
 class HelloWorldController {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @RequestMapping("/query")
+    @ResponseBody
+    public Map<String, Object> query() {
+        List<Map<String, Object>> list = jdbcTemplate.queryForList("select * from category where cid=?", "c002");
+        return list.get(0);
+    }
+
 
     @ResponseBody
     @RequestMapping("/hello")
