1d925d9df5ded19bb793567dca470c6562b4857e
lzhao <zhao434@usc.edu>
2019-02-11 16:02:27 -0800

    security start

---
+++ springbootLearn/security/src/main/java/com/lzhao/security/config/MySecurityConfig.java
@@ -0,0 +1,50 @@
+package com.lzhao.security.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        super.configure(http);
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/level1/**").hasRole("VIP1")
+                .antMatchers("/level2/**").hasRole("VIP2")
+                .antMatchers("/level3/**").hasRole("VIP3")
+                ;
+        http.formLogin()
+                .loginProcessingUrl("/login")
+                .failureUrl("/login?error")
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                ;
+        // 1. /login to login page
+        // 2. redirect to /login?error means login failed
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        super.configure(auth);
+        auth.inMemoryAuthentication()
+                .withUser("zhangsan").password(passwordEncoder().encode("12345")).roles("VIP1", "VIP2")
+                .and()
+                .withUser("lisi").password(passwordEncoder().encode("12345")).roles("VIP2", "VIP3")
+                ;
+    }
+}
